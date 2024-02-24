fa529f0165055fa46305560f45ab7d0837e4f3de
gkprasanth <gkpc2004@gmail.com>
2024-02-24 18:45:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+ 
+
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       <h1 className="text-3xl font-bold text-red-500 underline">
+      Inventory management
+    </h1>
+    </>
+  )
+}
+
+export default App
