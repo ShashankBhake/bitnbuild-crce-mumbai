347081501e25bdac0549e5a39a69e89324513122
piyal-nagda <136745417+piyal-nagda@users.noreply.github.com>
2024-02-25 04:59:12 +0530

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DrawerExample from './components/Drawer';
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom'
+import Flipcard from './login';
 import Home from './pages/Home';
-
 function App() {
   const isDarkMode = useSelector((state) => state.ui.isDarkMode);
 
@@ -16,6 +16,7 @@ function App() {
         <DrawerExample />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path={'/login'} element={<Flipcard />} />
         </Routes>
       </BrowserRouter>
     
