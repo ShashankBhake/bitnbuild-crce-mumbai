e389e044b348cbe8b849f22ec57c6964a4fea733
gkprasanth <gkpc2004@gmail.com>
2024-02-24 22:55:30 +0530

    Chakra dark mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { store } from './store'
+import { Provider } from 'react-redux'
+import { ColorModeScript, ChakraProvider, theme } from '@chakra-ui/react';
+import ColorModeSwitcher from './ColorModeSwitcher'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <Provider store={store} >
+    <React.StrictMode>
+    <ColorModeScript />
+      <ChakraProvider theme={theme}>
+      <ColorModeSwitcher />
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  </Provider>
 )
