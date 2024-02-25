15c30dff6362da1b4a126ec55c3b265796224586
Shashank Bhake <shashankbhake123@gmail.com>
2024-02-25 13:59:14 +0530

    fix

--- backend/router/api.js
+++ backend/router/api.js
@@ -7,7 +7,9 @@ var cors = require("cors");
 const userModel = require("../db/models/user");
 const axios = require("axios");
 const InventoryItem = require("../db/models/inventoryitems");
-const fs = require('fs');
+const fs = require("fs");
+const Sales = require("../db/models/sales");
+const Vendors = require("../db/models/vendors");
 
 // var allowlist = ['http://localhost:5173/', 'https://mscs.vercel.app/']
 // var corsOptionsDelegate = function (req, callback) {
@@ -32,14 +34,11 @@ myHeaders.append(
 );
 myHeaders.append("Content-Type", "application/json; charset=utf-8");
 
-
-
-
-router.get('/document/summarize', async (req, res) => {
+router.get("/document/summarize", async (req, res) => {
     try {
         const readFileAsBase64 = (filePath) => {
             try {
-                const fileContent = fs.readFileSync(filePath, 'base64');
+                const fileContent = fs.readFileSync(filePath, "base64");
                 return fileContent;
             } catch (err) {
                 console.error(err);
@@ -47,29 +46,29 @@ router.get('/document/summarize', async (req, res) => {
             }
         };
 
-        const filePath = '/Users/shashankhkanni/Desktop/Shashankh_Kanni.pdf';
+        const filePath = "/Users/shashankhkanni/Desktop/Shashankh_Kanni.pdf";
         const base64Content = readFileAsBase64(filePath);
-        console.log("file path", filePath, "base64", !!base64Content)
+        console.log("file path", filePath, "base64", !!base64Content);
         if (base64Content) {
             const req = {
-                "skipHumanReview": true,
-                "rawDocument": {
-                    "mimeType": "application/pdf",
-                    "content": base64Content
-                }
+                skipHumanReview: true,
+                rawDocument: {
+                    mimeType: "application/pdf",
+                    content: base64Content,
+                },
             };
-            fs.writeFileSync('request.json', JSON.stringify(req));
+            fs.writeFileSync("request.json", JSON.stringify(req));
             const curlCommand = `curl -X POST \
             -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
             -H "Content-Type: application/json; charset=utf-8" \
             -d @request.json \
             "https://us-documentai.googleapis.com/v1/projects/951555931380/locations/us/processors/c39f0f36a0c7e879:process"`;
-            const { promisify } = require('util');
-            const exec = promisify(require('child_process').exec);
+            const { promisify } = require("util");
+            const exec = promisify(require("child_process").exec);
 
             try {
                 await exec(curlCommand);
-                console.log('done');
+                console.log("done");
                 return res.status(200).json({
                     message: "Success",
                     code: "SUCCESS",
@@ -79,46 +78,43 @@ router.get('/document/summarize', async (req, res) => {
                 return res.status(500).json({
                     message: "Server Error",
                     code: "FAIL",
-                    error: error.message
+                    error: error.message,
                 });
             }
-
         } else {
             console.error("FAILED TO READ FILE");
             return res.status(500).json({
                 message: "Error reading file",
-                code: "FAIL"
+                code: "FAIL",
             });
         }
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err);
         return res.status(500).json({
             message: "Server Error",
             code: "FAIL",
-            error: err
-        })
+            error: err,
+        });
     }
-})
+});
 
-router.post('/image/summarize', async (req, res) => {
+router.post("/image/summarize", async (req, res) => {
     try {
         const { image } = req.body;
 
         const raw = {
-            "instances": [
+            instances: [
                 {
-                    "image": {
-                        "bytesBase64Encoded": image
-                    }
-                }
+                    image: {
+                        bytesBase64Encoded: image,
+                    },
+                },
             ],
-            "parameters": {
-                "sampleCount": 1,
-                "language": "en"
-            }
+            parameters: {
+                sampleCount: 1,
+                language: "en",
+            },
         };
-
 
         const requestOptions = {
             method: "POST",
@@ -127,7 +123,10 @@ router.post('/image/summarize', async (req, res) => {
             redirect: "follow",
         };
 
-        const { response } = await axios.post("https://asia-southeast1-aiplatform.googleapis.com/v1/projects/quiet-subset-415315/locations/asia-southeast1/publishers/google/models/imagetext:predict", requestOptions)
+        const { response } = await axios.post(
+            "https://asia-southeast1-aiplatform.googleapis.com/v1/projects/quiet-subset-415315/locations/asia-southeast1/publishers/google/models/imagetext:predict",
+            requestOptions
+        );
         // fetch("https://asia-southeast1-aiplatform.googleapis.com/v1/projects/quiet-subset-415315/locations/asia-southeast1/publishers/google/models/imagetext:predict", requestOptions)
         //     .then((response) => {
         //         return res.status(200).json({
@@ -435,6 +434,88 @@ router.post("/inventoryItems", async (req, res) => {
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+router.get("/sales", async (req, res) => {
+    try {
+        const sales = await Sales.find().populate("item"); // Populate the 'item' field to get details from the InventoryItem model
+        res.json(sales);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+router.post("/sales", async (req, res) => {
+    try {
+        const { itemId, quantitySold, totalAmount } = req.body;
+
+        // Check if the itemId exists in InventoryItem
+        const inventoryItem = await InventoryItem.findById(itemId);
+        if (!inventoryItem) {
+            return res.status(404).json({ error: "Inventory item not found" });
+        }
+
+        // Create a new sale
+        const sale = new Sales({
+            item: itemId,
+            quantitySold,
+            totalAmount,
+        });
+
+        await sale.save();
+        res.status(201).json(sale);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+router.get("/vendors", async (req, res) => {
+    try {
+        const vendors = await Vendors.find().populate("productsSupplied"); // Populate the 'productsSupplied' field to get details from the InventoryItem model
+        res.json(vendors);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+router.post("/vendors", async (req, res) => {
+    try {
+        const {
+            name,
+            company,
+            contactNumber,
+            email,
+            address,
+            productsSupplied,
+        } = req.body;
+
+        // Check if the product IDs in productsSupplied exist in InventoryItem
+        const invalidProductIds = productsSupplied.filter(async (productId) => {
+            const inventoryItem = await InventoryItem.findById(productId);
+            return !inventoryItem;
+        });
+
+        if (invalidProductIds.length > 0) {
+            return res
+                .status(404)
+                .json({ error: "One or more products supplied not found" });
+        }
+
+        // Create a new vendor
+        const vendor = new Vendors({
+            name,
+            company,
+            contactNumber,
+            email,
+            address,
+            productsSupplied,
+        });
+
+        await vendor.save();
+        res.status(201).json(vendor);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
     }
 });
 
